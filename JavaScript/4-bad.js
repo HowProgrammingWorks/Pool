9edaf8f04162808f7f4f1bb2cb0f7646b6e6e516
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2025-02-24 13:52:47 +0200

    Add bad example

---
+++ JavaScript/4-bad.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const poolify = (factory, min, norm, max) => {
+  const duplicate = (n) => new Array(n).fill().map(() => factory());
+  const items = duplicate(norm);
+
+  return (item) => {
+    if (item) {
+      if (items.length < max) {
+        items.push(item);
+      }
+      console.log('Recycle item, count =', items.length);
+      return null;
+    }
+    if (items.length < min) {
+      const instances = duplicate(norm - items.length);
+      items.push(...instances);
+    }
+    const res = items.pop();
+    console.log('Get from pool, count =', items.length);
+    return res;
+  };
+};
+
+// Usage
+
+// Factory to allocate 4kb buffer
+const buffer = () => new Uint32Array(1024);
+
+// Allocate pool of 10 buffers
+const pool = poolify(buffer, 5, 10, 15);
+
+let i = 0;
+
+const next = () => {
+  const item = pool();
+  console.log('Buffer size', item.length * 32);
+  i++;
+  if (i < 20) {
+    setTimeout(next, i * 10);
+    setTimeout(() => pool(item), i * 100);
+  }
+};
+
+next();
